ca2472121e70a6d27e6b61b7e22c8f90324b673a
mariamaOlive <mariama.oliveira@yahoo.com.br>
2025-10-21 01:21:30 +0000

    Implementing incremental load

---
+++ utils/1.prepare_for_incremental_load.sql
@@ -0,0 +1,25 @@
+-- Databricks notebook source
+-- MAGIC %md
+-- MAGIC ##### Drop all the tables
+
+-- COMMAND ----------
+
+DROP DATABASE IF EXISTS f1_processed CASCADE;
+
+-- COMMAND ----------
+
+CREATE DATABASE IF NOT EXISTS f1_processed
+LOCATION "abfss://processed@formula1dlmariama.dfs.core.windows.net/";
+
+-- COMMAND ----------
+
+DROP DATABASE IF EXISTS f1_presentation CASCADE;
+
+-- COMMAND ----------
+
+CREATE DATABASE IF NOT EXISTS f1_presentation 
+LOCATION "abfss://presentation@formula1dlmariama.dfs.core.windows.net/"
+
+-- COMMAND ----------
+
+
